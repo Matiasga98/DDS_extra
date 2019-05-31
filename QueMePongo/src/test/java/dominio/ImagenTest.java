ca24ed5f5a79bc3adbdb5c93b39528208d137615
iofel <eliofloreschoque@est.frba.utn.edu.ar>
2019-05-31 14:23:12 -0300

    Se agrega test para la clase imagen... (leer comentario)

---
+++ QueMePongo/src/test/java/dominio/ImagenTest.java
@@ -0,0 +1,44 @@
+package dominio;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ImagenTest {
+
+    Imagen imagen;
+
+    String direccionDeOrigenDeImagen;
+    String direccionDeDestinoDeImagen;
+
+    int anchoNuevo;
+    int largoNuevo;
+
+    @Before
+    public void setUp() throws Exception {
+
+        anchoNuevo = 200;
+        largoNuevo = 200;
+
+        //MODIFICAR LA LOCACION EN PARA SU COMPU EN CADA CASO
+        direccionDeOrigenDeImagen = "../../imagenes/campera.png";
+        direccionDeDestinoDeImagen = "../../imagenes/campera-normalizada.png";
+
+        imagen = new Imagen();
+
+        imagen.setAnchoDeDestino(anchoNuevo);
+        imagen.setLargoDeDestino(largoNuevo);
+
+    }
+
+    @Test
+    public void cargarImagen() throws IOException {
+        imagen.cargarImagen(direccionDeOrigenDeImagen,direccionDeDestinoDeImagen);
+        File imagenGuardada = new File(direccionDeDestinoDeImagen);
+        assertNotNull(imagenGuardada);
+    }
+}
